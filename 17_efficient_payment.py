ad05f40e98804d2e318278ac7d1ab105849e0e61
Larissa Triess <larissa.triess@daimler.com>
2018-12-21 22:17:13 +0100

    Fix rounding error
    Branch: master

--- 17_efficient_payment.py
+++ 17_efficient_payment.py
@@ -2,26 +2,8 @@ import sys
 
 DEBUG = False
 
-denomination = [0.01, 0.02, 0.05, 0.10, 0.20, 0.50, 1.00, 2.00, 5.00, 10.00, 20.00, 50.00, 100.00, 200.00, 500.00]
-
-
-def largest(rest_value, index, division):
-    if rest_value < int(denomination[index] * 100.0):
-        largest(rest_value, index - 1, division)
-    elif rest_value == int(denomination[index] * 100.0):
-        division.append(denomination[index])
-        s = ''
-        for num in list(reversed(sorted(division))):
-            if num % 1 == 0:
-                s += '{} '.format(int(num))
-            else:
-                s += '{0:.2f} '.format(num)
-        print(s[:-1])
-    elif rest_value > int(denomination[index] * 100.0):
-        division.append(denomination[index])
-        largest(rest_value - int(denomination[index] * 100.0), index, division)
-    else:
-        return
+denom = [1, 2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000]
+strval = ['0.01', '0.02', '0.05', '0.10', '0.20', '0.50', '1', '2', '5', '10', '20', '50', '100', '200', '500']
 
 
 def main():
@@ -30,12 +12,25 @@ def main():
         sys.stdin = open("samples/17_input.txt")
 
     for _ in range(int(input())):
-        value = float(input())
+        value = int(round(100 * float(input())))
 
-        if value == 0.0:
+        if value == 0:
             print()
         else:
-            largest(int(value * 100.0), len(denomination) - 1, [])
+            idx = len(denom) - 1
+            outstr = ''
+            while True:
+                if value > denom[idx]:
+                    outstr += '{} '.format(strval[idx])
+                    value -= denom[idx]
+                elif value == denom[idx]:
+                    outstr += strval[idx]
+                    print(outstr)
+                    break
+                elif value < denom[idx]:
+                    idx -= 1
+                else:
+                    raise ValueError
 
 
 if __name__ == "__main__":
